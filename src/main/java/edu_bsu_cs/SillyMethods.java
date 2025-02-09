7df99735fb011d130ef98962f810724e232d8c02
Drat09 <brian.kelly@bsu.edu>
2025-02-09 11:22:06 -0500

    New Silly Commit

--- src/main/java/edu_bsu_cs/SillyMethods.java
+++ src/main/java/edu_bsu_cs/SillyMethods.java
@@ -2,8 +2,29 @@ package edu_bsu_cs;
 
 public class SillyMethods {
     public boolean didGuess42(int i){
-        return true;
+        if(i == 42){
+            return true;
+        }
+        else{
+            return false;
+        }
+
     }
-    public boolean countTo(int i){
-        return true;
-}}
+
+    public String countTo(int i) {
+        String counting = "";
+        for(int a = 0; a <= i; a++){
+            if(a < i) {
+                counting = counting + Integer.toString(a) + " ";
+            }
+            else{
+                counting = counting + Integer.toString(a);
+            }
+
+        }
+        return counting;
+    }
+}
+
+
+
